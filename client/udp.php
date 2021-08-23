fdea01be793e6dd38716f806870be94fb3783bf4
dxing1202 <dxing1202@gmail.com>
2021-08-23 18:59:03 +0800

    Test Http Server

--- client/udp.php
+++ client/udp.php
@@ -1,22 +1,18 @@
 <?php
 
-# 测试连接TCP Swoole 服务器
+# 测试 TCP Swoole Client
+# UDP客户端不需要连接服务，直接根据ip/端口发送
 $client = new \Swoole\Client(SWOOLE_SOCK_UDP);
-// $coonetBool = $client->connect('127.0.0.1', 9502);
-// if (!$coonetBool) {
-//     echo '连接失败';
-//     exit;
-// }
 
 // php cli常量
 fwrite(STDOUT, "请输入消息：");
 $msg = trim(fgets(STDIN));
 
-// 发送消息给 tcp server 服务器
+// 发送消息给 UDP server 服务器
 $client->sendto('127.0.0.1', 9502, $msg);
 
 // 接受来自server的数据
 $result = $client->recv();
 echo $result;
 
-// $client->close();
+$client->close();
