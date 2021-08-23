352baf7f4e6a1662d2e6aa2f0b1a37964ccdb264
dxing1202 <dxing1202@gmail.com>
2021-08-23 15:58:46 +0800

    1

--- client/udp.php
+++ client/udp.php
@@ -13,8 +13,10 @@ fwrite(STDOUT, "请输入消息：");
 $msg = trim(fgets(STDIN));
 
 // 发送消息给 tcp server 服务器
-$client->send($msg);
+$client->sendto($msg);
 
 // 接受来自server的数据
 $result = $client->recv();
 echo $result;
+
+// $client->close();
