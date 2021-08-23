bb47103d9e6aa43bd978d584dbf27be450cd4243
dxing1202 <dxing1202@gmail.com>
2021-08-23 15:20:27 +0800

    test webhook

---
+++ client/udp.php
@@ -0,0 +1,20 @@
+<?php
+
+# 测试连接TCP Swoole 服务器
+$client = new \Swoole\Client(SWOOLE_SOCK_UDP);
+$coonetBool = $client->connect('127.0.0.1', 9502);
+if (!$coonetBool) {
+    echo '连接失败';
+    exit;
+}
+
+// php cli常量
+fwrite(STDOUT, "请输入消息：");
+$msg = trim(fgets(STDIN));
+
+// 发送消息给 tcp server 服务器
+$client->send($msg);
+
+// 接受来自server的数据
+$result = $client->recv();
+echo $result;
