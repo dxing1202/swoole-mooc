e34672dbd78925e45908e469993c4a85ff2d416e
dxing1202 <dxing1202@gmail.com>
2021-08-24 16:41:35 +0800

    1

--- server/ws.php
+++ server/ws.php
@@ -11,7 +11,7 @@ $ws->on('open', function ($ws, $request) {
     // Log记录 连接来访
     $serverInfo = $request->server;
     $date = date('Y-m-d H:i:s', $serverInfo["request_time"]);
-    $log = sprintf("Client: %i, Time: %s IP: %s", $request->fd, $date, $serverInfo['remote_addr']);
+    $log = sprintf("Client: %s, Time: %s IP: %s", $request->fd, $date, $serverInfo['remote_addr']);
     echo $log;
 
     // var_dump($request);
