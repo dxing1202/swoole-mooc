f993003973f84da45ffeac64acd55f9e3163dcd5
dxing1202 <dxing1202@gmail.com>
2021-08-24 16:24:39 +0800

    1

--- server/ws.php
+++ server/ws.php
@@ -9,7 +9,7 @@ $ws = new Swoole\WebSocket\Server('0.0.0.0', 9502);
 //监听WebSocket连接打开事件
 $ws->on('open', function ($ws, $request) {
     echo date('Y-m-d H:i:s');
-    dump($request);
+    var_dump($request);
     $ws->push($request->fd, "hello, welcome\n");
 });
 
