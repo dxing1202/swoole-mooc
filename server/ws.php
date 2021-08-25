7aac6666f79c572c55d08fd28a630f7d4f7801d2
dxing1202 <dxing1202@gmail.com>
2021-08-25 14:41:04 +0800

    1

--- server/ws.php
+++ server/ws.php
@@ -1,40 +1,75 @@
 <?php
 
-//创建WebSocket Server对象，监听0.0.0.0:9502端口
-$ws = new Swoole\WebSocket\Server('0.0.0.0', 9502);
+/**
+ * WebSocket Swoole Server Class
+ * @author dxing1202
+ * @copyright dxing1202.cn
+ * @since 2021-08-25
+ */
+class Ws
+{
+    // Swoole\Server对象
+    protected $server = null;
+    // 监听主机 对应外网的IP 0.0.0.0监听所有ip
+    protected $host = '0.0.0.0';
+    // 监听端口号 
+    protected $port = 9502;
 
-// 设置 ws server 配置
-// $ws->set([]);
-// 配置支持HTTP服务
-$ws->set([
-    'enable_static_handler' => true,
-    'document_root' => '/www/wwwroot/swoole-mooc/data'
-]);
+    public function __construct()
+    {
+        //创建WebSocket Server对象
+        $this->server = new Swoole\WebSocket\Server($this->host, $this->port);
 
-//监听WebSocket连接打开事件
-$ws->on('open', function ($ws, $request) {
-    // Log记录 连接来访
-    $serverInfo = $request->server;
-    $date = date('Y-m-d H:i:s', $serverInfo["request_time"]);
-    $log = sprintf("Client:%s Time:%s IP:%s Connection successful\n", $request->fd, $date, $serverInfo['remote_addr']);
-    echo $log;
+        $this->server->on('open', [$this, 'onOpen']);
+        $this->server->on('message', [$this, 'onMessage']);
+        $this->server->on('disconnect', [$this, 'onDisconnect']);
 
-    // var_dump($request);
-    $ws->push($request->fd, "hello, welcome\n");
-});
+        $this->server->start();
+    }
 
-//监听WebSocket消息事件
-$ws->on('message', function ($ws, $frame) {
-    echo "Message: {$frame->data}\n";
-    $ws->push($frame->fd, "server: {$frame->data}");
-});
+    /**
+     * onOpen 监听WS连接事件
+     * @param  object $server Swoole\WebSocket\Serve 服务对象
+     * @param  object $request Swoole\Http\Request 请求对象信息
+     * @return void   无
+     */
+    protected function onOpen($server, $request)
+    {
+        # Log记录 连接连接
+        // 请求时间
+        $date = date('Y-m-d H:i:s', $request->server["request_time"]);
+        $log = sprintf("Client:%s Time:%s IP:%s Connection successful\n", $request->fd, $date, $request->server['remote_addr']);
+        echo $log;
 
-//监听WebSocket连接关闭事件
-$ws->on('close', function ($ws, $fd) {
-    // 当前时间
-    $date = date('Y-m-d H:i:s');
-    $log = sprintf("Client:%s Time:%s Disconnected\n", $fd, $date);
-    echo $log;
-});
+        // var_dump($request);
+        $server->push($request->fd, "hello, welcome\n");
+    }
 
-$ws->start();
+    /**
+     * onMessage 监听WS消息事件
+     * @param  object $server Swoole\WebSocket\Serve 服务对象
+     * @param  object $frame  Swoole\WebSocket\Frame 客户端发来的数据帧信息对象
+     * @return void   无
+     */
+    protected function onMessage($server, $frame) {
+        echo "Message: {$frame->data}\n";
+        $server->push('Client:' . $frame->fd, " Server-push:{$frame->data} Time:" . date('Y-m-d H:i:s'));
+    }
+
+    /**
+     * onDisconnect 监听WebSocket连接关闭事件
+     * @param  object $server Swoole\WebSocket\Server 服务对象
+     * @param  object $fd     客户端ID
+     * @return void   无
+     */
+    protected function onDisconnect($server, $fd)
+    {
+        # Log记录 关闭事件
+        // 当前时间
+        $date = date('Y-m-d H:i:s');
+        $log = sprintf("Client:%s Time:%s Disconnected\n", $fd, $date);
+        echo $log;
+    }
+}
+
+$ws = new Ws();
