ae726de4387761200e1daba7c2c29a582774abb3
dxing1202 <dxing1202@gmail.com>
2021-08-27 17:15:09 +0800

    1

--- io/read.php
+++ io/read.php
@@ -8,28 +8,24 @@ $filePath = __DIR__ . "/1.txt";
 # 设置加载协程Hook 
 Co::set(['hook_flags' => SWOOLE_HOOK_FILE]);
 
-$co = Co\run(function () use ($filePath) {
+Co\run(function () use ($filePath) {
 
 	# 协程文件操作 4.3+版本后不推荐 【推荐使用一键协程化 Hook】
 	// var_dump( Co\System::statvfs('/') );
 	
 	# 协程方式读取文件。
-	$r = Co\System::readFile($filePath);
-	var_dump($r);	
+	// $r = Co\System::readFile($filePath);
+	// var_dump($r);
 
 	# 【一键协程化 Hook】
 	// var_dump(__DIR__); // /www/wwwroot/swoole-mooc/io
     
-    // $fp = fopen("1.txt", "r");
-   	// $content = fread($fp, filesize($filePath));
-   	// echo $content . PHP_EOL;
-   	// fclose($fp);
+    $fp = fopen("1.txt", "r");
+   	$content = fread($fp, filesize($filePath));
+   	echo $content . PHP_EOL;
+   	fclose($fp);
 
-
+   	// 测试顺序运行
+	echo "start" . PHP_EOL;
 
 });
-
-var_dump($co);
-
-// 测试顺序运行
-echo "start" . PHP_EOL;	
