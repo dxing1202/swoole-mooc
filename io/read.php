d2de6cb7749ff25e58cabfba8238792012e37d2a
dxing1202 <dxing1202@gmail.com>
2021-08-27 12:18:32 +0800

    1

--- io/read.php
+++ io/read.php
@@ -3,14 +3,17 @@
 Co::set(['hook_flags' => SWOOLE_HOOK_FILE]);
 
 Co\run(function () {
-    $fp = fopen("1.txt", "r");
-    // fwrite($fp, str_repeat('A', 2048));
-    // fwrite($fp, str_repeat('B', 2048));
-    var_dump(__DIR__);
+
+	var_dump(__DIR__);
     $file = __DIR__ . "/1.txt";
     echo $file . PHP_EOL;
     echo filesize($file) . PHP_EOL;
-   	$content = fread($file, filesize($file));
+    $fp = fopen("1.txt", "r");
+    // fwrite($fp, str_repeat('A', 2048));
+    // fwrite($fp, str_repeat('B', 2048));
+    
+    
+   	$content = fread($fp, filesize($file));
    	echo $content;
-   	fclose($file);
+   	fclose($fp);
 });
